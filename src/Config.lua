9349d794bc59a0bd00c90086c6fc474c39a2daa6
FlynnFarrow <56035537+FlynnFarrow@users.noreply.github.com>
2021-12-23 16:11:04 +0000

    Changed icons and changed dead links
    @csqrl pls do not forget to update developer marketplace version, that is still on a version where rprxy is still in http requests

--- src/Config.lua
+++ src/Config.lua
@@ -1,9 +1,9 @@
 return {
     PluginId = 336673829,
-    PluginIcon = "rbxassetid://4641524854",
+	PluginIcon = "rbxassetid://8325760954",
     PluginGUID = "csqrl.plugin.plugin-importer",
     PluginSettingsKey = "2B605F1C627243798979D8221C971C14",
 
     DefaultLocale = "en-gb",
-    Version = "3.0.8",
+    Version = "3.0.9",
 }
